e71e7367b4fdecca6377d3ed9cdf83fd841f421e
Shav <shavenchew@yahoo.com>
2017-01-14 12:44:50 -0800

    Working Post Route to search for a new stock. The previous stock gets removed

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var express     = require('express'),
     Highcharts  = require('highcharts'),
     quandlKey   = require('./apiKeys'),
     Quandl      = require('quandl'),
+    bodyParser  = require('body-parser'),
     app         = express();
 
 app.set('view engine', 'ejs');
 
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 //=========QUANDL CONFIG========
@@ -18,9 +20,35 @@ var quandl = new Quandl({
 })
 
 app.get('/', function(req, res){
+  var stockName = 'FB';
   quandl.dataset({
   source: "WIKI",
-  table: "FB"
+  table: stockName
+}, {
+  start_date: "2016-01-01",
+  end_date: "2016-12-30",
+  column_index: 4
+}, function(err, stockData){
+  if(err){
+    console.log(err);
+  } else {
+  var stock = JSON.parse(stockData).dataset;
+ //  var highData = stock.data.map(function(d){
+ //   return [new Date(d[0]).getTime(), d[1]];
+ // });
+ stockPrices = stock.data.reverse();
+  console.log(stockPrices);
+  res.render('landing', {stockPrices : stockPrices, stockName : stockName});
+      }
+    });
+});
+
+
+app.post('/', function(req, res){
+  var stockName = req.body.stockName;
+  quandl.dataset({
+  source: "WIKI",
+  table: stockName
 }, {
   start_date: "2016-01-01",
   end_date: "2016-12-30",
@@ -32,10 +60,9 @@ app.get('/', function(req, res){
  // });
  stockPrices = stock.data.reverse();
   console.log(stockPrices);
-  res.render('landing', {stockPrices : stockPrices});
-});
-
-});
+  res.render('landing', {stockPrices : stockPrices, stockName: stockName});
+  });
+})
 
 app.listen('9000', function(){
   console.log('Stock Chart Starting!');
