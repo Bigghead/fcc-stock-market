0ab06f414a4e9ed80e8d7e22dc5d13203324e459
Bigghead <shavenchew@yahoo.com>
2018-07-25 17:54:03 -0700

    Why is jQuery here????

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var mongoUser = process.env.mongoUser || mongoUser,
     Key        = process.env.Key || Key,
     port       = process.env.PORT || 9000;
 
-console.log('User ' + mongoUser);
 //=======DATABASE=====
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://'+ mongoUser +':'+ mongoPass +'@ds111469.mlab.com:11469/fcc-stocks');
@@ -41,19 +40,10 @@ app.use(function(req, res, next){
 app.use(Method('_method'));
 
 
-//=========QUANDL CONFIG========
-var quandl = new Quandl({
-  auth_token: Key,
-  api_version: 3
-})
-
-
 
 //==============SHOW ALL STOCK LINES ON CHART=====
 app.get('/', function(req, res){
 
-  // axios.get(`https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json?api_key${keys.Key}&start_date=2001-01-01&end_date=2018-07-23&order=asc&order=asc&column_index=4`)
-  //      .then( d => res.json(d.data.dataset))
   Stocks.find({}, function(err, foundStocks){
     if(err){
       console.log(err);      
@@ -67,7 +57,7 @@ app.get('/', function(req, res){
 
 //===========ADD A NEW STOCK ONTO CHART=======
 app.post('/', async(req, res) => {
-  const stockName = req.body.stockName.toUpperCase();
+  const { stockName } = req.body;
 
   //Check if we have the stock in db
   //If yes, redirect, don't let them add the same stock
@@ -90,14 +80,11 @@ app.post('/', async(req, res) => {
     
   } catch( e ) {
 
-    console.log(e);
     res.redirect('/');
 
   }
 
-
 });
-
 
 
 //==========REMOVE STOCK FROM HOMEPAGE CHART========
